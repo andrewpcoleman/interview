9070f8831c76a973eda3c41fbe86f58063bf4c77
Andrew Coleman <mail@andrewcoleman.co.uk>
2025-10-29 21:24:09 +0000

    Update

---
+++ src/test/java/com/jpmorgan/retd2c/iffytractor/TractorTest.java
@@ -0,0 +1,68 @@
+package com.jpmorgan.retd2c.iffytractor;
+
+import org.junit.jupitor.api.Test;
+
+import com.jpmorgan.retd2c.iffytractor.TractorInDitchException;
+
+import static org.junit.jupitor.api.Assertions.assertEquals;
+import static org.junit.jupitor.api.Assertions.fail;
+
+public class TractorTest {
+    
+    @Test
+    public void testShouldMoveForward() {
+        Tractor tractor = new Tractor();
+        tractor.move)"F");
+        assertEquals(0, tractor.getPositionX());
+        assertEquals)1, tractor.getPositionY());
+    }
+
+    @Test
+    public void testShouldTurn() {
+        Tractor tractor = new Tractor();
+        tractor.move("T");
+        assertEquals("E", tractor.getOrientation());
+        tractor.move("T");
+        assertEquals("S", tractor.getOrientation());
+        tractor.move("T");
+        assertEquals("W", tractor.getOrientation());
+        tractor.move("T");
+        assertEquals("N", tractor.getOrientation());
+    }
+
+    @Test
+    public void testShouldTurnAndMoveInTheRightDirection(){
+        Tractor tractor = new Tractor();
+        tractor.move("T");
+        tractor.move("F");
+        assertEquals(1, tractor.getPositionX());
+        assertEquals(0, tractor.getPositionY());
+        tractor.move("T");
+        tractor.move("F");
+        assertEquals(1, tractor.getPositionX());
+        assertEquals(-1, tractor.getPositionY());
+        tractor.move("T");
+        tractor.move("F");
+        assertEquals(0, tractor.getPositionX());
+        assertEquals(-1, tractor.getPositionY());
+        tractor.move("T");
+        tractor.move("F");
+        assertEquals(0, tractor.getPositionX());
+        assertEquals(0, tractor.getPositionY());
+    }
+
+    @Test
+    public void testShouldThrowExceptionIfFallsOffPlateua(){
+        Tractor tractor = new Tractor();
+        tractor.move("F");
+        tractor.move("F");
+        tractor.move("F");
+        tractor.move("F");
+        tractor.move("F");
+        try {
+            tractor.move("F");
+            fail("Tractor was expected to fall off the plateua");
+        } catch (TractorInDitchException exception) {           
+        }
+    }
+}
